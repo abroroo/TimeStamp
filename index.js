4584d3f82324c71f1db3e1deadb7cffdd86485ea
abroroo <abror_iskandarov@yahoo.com>
2022-06-23 21:27:36 +0900

    test cases met

--- index.js
+++ index.js
@@ -29,42 +29,24 @@ app.get("/api/hello", (req, res) => {
 
 // whenever there is : it is a url parameter, and we can access it by req.params
 app.get(`/api/:input`, (req, res) => {
-    let input = req.params.input;
-    let unix;
-    let utc;
-    
 
-    if (input.includes('-') && !input.startsWith('-')){
-      unix = new Date(input).getTime();
-      utc = new Date(input).toUTCString();
-    
-    } else if (input.startsWith('-') && input.includes([0-9]) || !isNaN(input)){
-      unix = parseInt(input);
-      utc = new Date(unix).toUTCString();
+   let input = new Date()
+// check if input is given, otherwise input is empty return current date
+   if (req.params.input) {
+    // parser input into integers 
+    let inputInt = +req.params.input;
+// check is input is number if not use parsed input as a value of input
+    input = isNaN(inputInt) ? new Date(req.params.input) : new Date(inputInt)
+// check if input is valid date, if not return error object
+    if(!(input instanceof Date) || isNaN(input.getTime())) return res.json({error: "Invalid Date"})
 
-    } else if (isNaN(unix)){
-      res.send({
-        error : "Invalid Date"
-      })
-    }
-
-    res.send({
-      unix: unix,
-      utc: utc
-    });
-     
+   }
+   // return json object with a value of input provided
+   return res.json({unix: input.getTime(), utc: input.toUTCString() })
   }
 );
 
-app.get('/api', (req, res) => {
-  let unix = new Date().getTime();
-  let utc = new Date().toUTCString();
 
-  res.send({
-    unix: unix,
-    utc: utc
-  })
-})
 
 const port = process.env.PORT || 3000 ;
 
@@ -72,3 +54,42 @@ const port = process.env.PORT || 3000 ;
 var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+
+
+
+// let input = req.params.input;
+// let unix;
+// let utc;
+
+
+// if (input.includes('-') && !input.startsWith('-')){
+//   unix = new Date(input).getTime();
+//   utc = new Date(input).toUTCString();
+
+// } else if (input.startsWith('-') && input.includes([0-9]) || !isNaN(input)){
+//   unix = parseInt(input);
+//   utc = new Date(unix).toUTCString();
+
+// } else if (isNaN(unix)){
+//   res.send({
+//     error : "Invalid Date"
+//   })
+// }
+
+// res.send({
+//   unix: unix,
+//   utc: utc
+// });
+ 
+// app.get('/api', (req, res) => {
+//   let unix = new Date().getTime();
+//   let utc = new Date().toUTCString();
+
+//   res.send({
+//     unix: unix,
+//     utc: utc
+//   })
+// })
